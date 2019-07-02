fe5db3701a4f346c637ec8cff890e0f214510719
JS Park <parkjjss@gmail.com>
2019-07-02 15:52:53 +0900

    정규식 1.1

---
+++ pethub-batch/src/test/java/kr/pethub/RegEx.java
@@ -0,0 +1,19 @@
+package kr.pethub;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class RegEx {
+	
+	 Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Test
+	public void 숫자만_체크() {
+		
+		logger.debug("-------------------------------------------------------------------------------> start");
+		
+		logger.debug("-------------------------------------------------------------------------------> end");
+	}
+}
