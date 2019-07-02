92ed51c940d31c5daa7184ef77a336c69ab6faa8
jspark <parkjjss2@gmail.com>
2019-07-02 21:18:43 +0900

    Jsoup  테스트 케이스

--- pethub-batch/src/test/java/kr/pethub/crawling/Pethub.java
+++ pethub-batch/src/test/java/kr/pethub/crawling/Pethub.java
@@ -1,5 +1,11 @@
 package kr.pethub.crawling;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -7,7 +13,8 @@ import kr.pethub.BaseTestCase;
 import kr.pethub.job.crawling.service.PethubService;
 
 
-public class Pethub extends BaseTestCase{
+//public class Pethub extends BaseTestCase{
+	public class Pethub {
 	
 	
 	@Autowired
@@ -16,10 +23,30 @@ public class Pethub extends BaseTestCase{
 	@Test
 	public void 데이터베이스_커넥션() {
 		
-		logger.debug("-------------------------------------------------------------------------------> start");
+		//logger.debug("-------------------------------------------------------------------------------> start");
 		
 		pethubService.getId();
 		
-		logger.debug("-------------------------------------------------------------------------------> end");
+		//logger.debug("-------------------------------------------------------------------------------> end");
+	}
+	
+	
+	@Test
+	public void JSOUP_기본() {
+
+		try {
+			
+			Document doc = Jsoup.connect("https://m.stock.naver.com/searchItem.nhn?searchType=init").timeout(5000).get();
+			Elements contents = doc.select("#searchResult li");
+			
+			for( Element ele :  contents) {
+				System.out.println( ele.getElementsByClass("stock_item").text() +" :  "+ ele.getElementsByClass("stock_price").text());
+			}
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
