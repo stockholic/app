a788ba10261bbe5fff027447bb94fe1af1a88b99
JS Park <parkjjss@gmail.com>
2019-07-02 17:16:37 +0900

    정규식 테스트 케이스 작성

--- pethub-batch/src/test/java/kr/pethub/RegEx.java
+++ pethub-batch/src/test/java/kr/pethub/RegEx.java
@@ -1,5 +1,8 @@
 package kr.pethub;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +17,146 @@ public class RegEx {
 		
 		logger.debug("-------------------------------------------------------------------------------> start");
 		
+		String pattern ="^[0-9]*$";
+		
+		String str1 = "1234";
+		String str2 = "12sagd34";
+		
+		isValid(pattern, str1);
+		isValid(pattern, str2);
+		
 		logger.debug("-------------------------------------------------------------------------------> end");
 	}
+	
+	@Test
+	public void URL_패턴1() {
+		
+		logger.debug("-------------------------------------------------------------------------------> start");
+		
+		String str = "onClick=window.open('/abcd/xyz.php?aa=11&bb+22&cc=','','width=200;height=500')".trim();
+		
+		// (?!) 뒤에 대소문자 안가림
+		String pattern ="(onClick=window\\.open\\()'([^\\s']+)'(.*)";
+		
+		isRegexElement(pattern, str);
+		
+		String prefix = "http://www.pethub.kr";
+		String surfix = str.replaceAll(pattern, "$2");
+		
+		logger.debug("Link : {} ",  prefix + surfix);
+		
+		logger.debug("-------------------------------------------------------------------------------> end");
+	}
+	
+
+	@Test
+	public void URL_패턴2() {
+		
+		logger.debug("-------------------------------------------------------------------------------> start");
+		
+		String str = "<a href='/abcd/def.php?aa=11&bb=22' target='_blank'>link</a>".trim();
+		
+		// (.*) 모든문자
+		String pattern ="(<a href=)'([^\\s']+)'(.*)";
+
+		isRegexElement(pattern, str);
+		
+		String prefix = "http://www.pethub.kr";
+		String surfix = str.replaceAll(pattern, "$2");
+		
+		logger.debug("Link : {}  ",  prefix + surfix);
+		
+		logger.debug("-------------------------------------------------------------------------------> end");
+	}
+	
+	@Test
+	public void URL_패턴3() {
+		
+		logger.debug("-------------------------------------------------------------------------------> start");
+		
+		String str = "javascript:view(\"bbs\",\"2\")".trim();
+		
+		String pattern ="(javascript:view\\()\"([a-z]+)\",\"([0-9])\"(.*)";
+
+		isRegexElement(pattern, str);
+		
+		String prefix = "http://www.pethub.kr";
+		String surfix = str.replaceAll(pattern, "/xyz.php?board=$2&no=$3");
+		
+		logger.debug("Link : {}  ",  prefix + surfix);
+		
+		logger.debug("-------------------------------------------------------------------------------> end");
+	}
+	
+	@Test
+	public void 이미지추출() {
+		
+		logger.debug("-------------------------------------------------------------------------------> start");
+		
+		String str = "<img src=\"/images/board/xxx.php?no=1234\" class=\"top_img\">".trim();
+		
+		String pattern ="(<img src=)\"([^\\s\"]+)\"(.*)";
+
+		isRegexElement(pattern, str);
+		
+		String prefix = "http://www.pethub.kr";
+		String surfix = str.replaceAll(pattern, "$2");
+		
+		logger.debug("Link : {}  ",  prefix + surfix);
+		
+		logger.debug("-------------------------------------------------------------------------------> end");
+	}
+	
+	
+	@Test
+	public void 태그제거() {
+		
+		logger.debug("-------------------------------------------------------------------------------> start");
+		
+		String str = "<a href='/abcd/def.php?aa=11&bb=22' target='_blank'>하하하</a> <호호호호><img src=\"/images/board/xxx.php?no=1234\" class=\\\"top_img\">".trim();
+		
+		String pattern ="<[\\/a-zA-Z]+[^>]+>";
+		isValid(pattern, str);
+		
+		logger.debug("Result : {}  ", str.replaceAll(pattern, ""));
+
+		
+		logger.debug("-------------------------------------------------------------------------------> end");
+	}
+	
+	
+	public boolean isValid(String pattern, String str) {
+		
+		boolean isValid = false;
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(str);
+		isValid = m.find();
+		
+		logger.debug("isMatch : {} ", isValid);
+		
+		
+		return isValid;
+		
+	}
+	
+	public boolean isRegexElement(String pattern, String str) {
+		
+		boolean isValid = false;
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(str);
+
+		if(m.matches()) {
+			logger.debug("Element found");
+			for(int i = 0; i < m.groupCount(); i++) {
+				logger.debug(i + " : {}", m.group(i));	
+			}
+			isValid = true;
+		}else {
+			logger.debug("Element not found");
+		}
+		
+		return isValid;
+		
+	}
+	
 }
